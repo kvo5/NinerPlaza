dcac4aa98901a368b573a180879e81bb585cb841
kvo5 <kvo5@uncc.edu>
2024-12-06 10:23:52 -0500

    Fix deployment configuration

--- studybud2/settings.py
+++ studybud2/settings.py
@@ -22,12 +22,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get('SECRET_KEY', 'django-insecure-26h+c1jbk_^yuhaio=1t=n_09ajs(j^@4hwjy#7#u34j6dwx)b')
+SECRET_KEY = os.environ.get('SECRET_KEY', 'your-default-secret-key-here')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = 'RENDER' not in os.environ
+DEBUG = os.environ.get('DEBUG', 'False') == 'True'
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 RENDER_EXTERNAL_HOSTNAME = os.environ.get('RENDER_EXTERNAL_HOSTNAME')
 if RENDER_EXTERNAL_HOSTNAME:
